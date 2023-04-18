071b0a2c248ac539af14b3cfe71b95b6500499f4
RG214221 <116347578+RG214221@users.noreply.github.com>
2023-04-18 18:06:43 +0300

    Update Program.cs

--- ToDoApi/Program.cs
+++ ToDoApi/Program.cs
@@ -18,6 +18,7 @@ var app = builder.Build();
 app.UseCors("corsapp");
 app.UseHttpsRedirection();
 app.MapGet("/items", async (ToDoDbContext context) => { return await context.Items.ToListAsync();} );
+app.MapGet("/",()=>"ToDoList Server is running");
 app.MapPost("/items", async (ToDoDbContext context, Item item) =>
 {
     EntityEntry<Item> itemToReturn = context.Items.Add(item); await context.SaveChangesAsync();
